4cc5c9f4953cd3ba377703d8c5e39b61da9c2c8c
Sahil <sahiljeet.singh@silverpush.co>
2019-08-08 12:49:13 +0530

    Feature: Added splash screen

--- app/src/main/java/com/labels/ui/activity/IntroSliderActivity.kt
+++ app/src/main/java/com/labels/ui/activity/IntroSliderActivity.kt
@@ -1,18 +1,28 @@
 package com.labels.ui.activity
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.widget.Toast
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
 import com.github.paolorotolo.appintro.AppIntro2
+import com.labels.MainActivity
 import com.labels.ui.fragments.intro.IntroFirstFragment
 import com.labels.ui.fragments.intro.IntroSecondFragment
 import com.labels.ui.fragments.intro.IntroThirdFragment
+import com.labels.utils.Constants.FIRST_START
 
 class IntroSliderActivity : AppIntro2() {
 
+    private lateinit var preference: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        preference= PreferenceManager.getDefaultSharedPreferences(baseContext)
+
 
         //val firstFragment: Fragment = IntroFirstFragment.newInstance()
         val secondFragment: Fragment = IntroSecondFragment.newInstance()
@@ -26,6 +36,10 @@ class IntroSliderActivity : AppIntro2() {
         isProgressButtonEnabled = true
     }
 
+    override fun onBackPressed() {
+        finish();
+    }
+
     override fun onSkipPressed(currentFragment: Fragment?) {
         super.onSkipPressed(currentFragment)
         Toast.makeText(this, "skip pressed", Toast.LENGTH_SHORT).show()
@@ -33,13 +47,16 @@ class IntroSliderActivity : AppIntro2() {
 
     override fun onDonePressed(currentFragment: Fragment?) {
         super.onDonePressed(currentFragment)
-        // Do something when users tap on Done button.
-        Toast.makeText(this, "Done pressed", Toast.LENGTH_SHORT).show()
+
+        val editor = preference.edit()
+        editor.putBoolean(FIRST_START, false).apply()
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 
     override fun onSlideChanged(@Nullable oldFragment: Fragment?, @Nullable newFragment: Fragment?) {
         super.onSlideChanged(oldFragment, newFragment)
-        // Do something when the slide changes.
         Toast.makeText(this, "Slide changed", Toast.LENGTH_SHORT).show()
     }
 }
